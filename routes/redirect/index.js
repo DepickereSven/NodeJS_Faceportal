9a3f1f873703145fefa52f1d6a392542bf34e192
svend <sven.depickere@student.howest.be>
2018-06-06 17:42:47 +0200

    added index redirect

---
+++ routes/redirect/index.js
@@ -0,0 +1,15 @@
+/**
+ Created by svend on 6/06/2018.
+ **/
+
+module.exports = (function () {
+
+    let normalIndex = function (res) {
+      res.render('index');
+    };
+
+    return {
+        normalIndex: normalIndex
+    }
+
+})();
