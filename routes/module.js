3c8024237c400b37feb7ad6d65310427fcccf39d
svend <sven.depickere@student.howest.be>
2018-06-06 18:57:42 +0200

    added /register

--- routes/module.js
+++ routes/module.js
@@ -2,11 +2,41 @@ const express = require('express');
 const router = express.Router();
 const knex = require('knex')(require('../Sql-data/sql-connection'));
 
+const sqlFunctions = require('./sqlFunctions/sqlFunctions');
+
 const index = require('./redirect/index');
 
 
 router.get('/', function(req, res, next) {
     index.normalIndex(res);
+});
+
+router.post('/register', function (req, res, next) {
+    let data = req.body;
+    console.log(data);
+    if (data.pass === data.passRepeat){
+        knex('login')
+            .where(
+                'emailAddress', data.email
+            )
+            .select('userID')
+            .then(function (userID) {
+                console.log(userID);
+                if (userID[0] === undefined){
+                    sqlFunctions.saveNewUser(knex, {
+                        htmlFunctions: index,
+                        userDetails: data,
+                        res: res
+                    })
+                }
+            })
+        
+    } else {
+        index.passNotCorrect(res, {
+            message: "Passwords don't match",
+            data
+        })
+    }
 });
 
 module.exports = router;
