ff9362bbbd2ea33a7797e03f2bb5d4e8f259e090
svend <sven.depickere@student.howest.be>
2018-06-06 18:56:46 +0200

    Added SQL functions

---
+++ routes/sqlFunctions/sqlFunctions.js
@@ -0,0 +1,31 @@
+/**
+ Created by svend on 6/06/2018.
+ **/
+
+
+module.exports = (function () {
+
+    let saveNewUser = function (knex, data) {
+        let index = data.htmlFunctions;
+        let userDetails = data.userDetails;
+        let res = data.res;
+        knex('login')
+            .insert(
+                [
+                    {
+                        userName: userDetails.user,
+                        emailAddress: userDetails.email,
+                        password: userDetails.pass
+                    }
+                ]
+            )
+            .then(
+                index.fillInLoginDetails(res, userDetails)
+            );
+    };
+
+    return {
+        saveNewUser: saveNewUser
+    }
+
+})();
