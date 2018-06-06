fd1b4696becb089ea7bab490bb63117b9067ba09
svend <sven.depickere@student.howest.be>
2018-06-06 18:57:30 +0200

    Updated redirecting to index

--- routes/redirect/index.js
+++ routes/redirect/index.js
@@ -2,14 +2,30 @@
  Created by svend on 6/06/2018.
  **/
 
+const render = require('./util/render');
+
+
+const i = 'index';
+
 module.exports = (function () {
 
     let normalIndex = function (res) {
-      res.render('index');
+        render.this(i, res)
+    };
+
+    let fillInLoginDetails = function (res, data) {
+        render.this(i, res, {
+            user: {
+                username: data.user,
+                password: data.pass
+            }
+
+        });
     };
 
     return {
-        normalIndex: normalIndex
+        normalIndex: normalIndex,
+        fillInLoginDetails: fillInLoginDetails
     }
 
 })();
