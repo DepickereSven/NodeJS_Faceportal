e898625aabe782bb10211156c015acc727afe851
svend <sven.depickere@student.howest.be>
2018-06-06 17:38:17 +0200

    Added SQL connection

---
+++ Sql-data/sql-connection.js
@@ -0,0 +1,17 @@
+/**
+ * Created by svend on 06/06/2018.
+ */
+
+module.exports = ({
+    client: 'mysql',
+    connection: {
+        host: "localhost",
+        port: 3306,
+        database: "faceportal",
+        user: "root",
+        password: ""
+    }
+});
+
+
+
