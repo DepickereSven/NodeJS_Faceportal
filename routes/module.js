f86d07b6145f25fcd12539a7fabee6c4861fd31b
svend <sven.depickere@student.howest.be>
2018-06-07 08:47:27 +0200

    Added more register functions

--- routes/module.js
+++ routes/module.js
@@ -7,33 +7,32 @@ const sqlFunctions = require('./sqlFunctions/sqlFunctions');
 const index = require('./redirect/index');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     index.normalIndex(res);
 });
 
 router.post('/register', function (req, res, next) {
     let data = req.body;
     console.log(data);
-    if (data.pass === data.passRepeat){
+    if (data.pass === data.passRepeat) {
         knex('login')
             .where('emailAddress', data.email)
             .orWhere('userName', data.user)
             .select('userID')
             .then(function (userID) {
-                if (userID[0] === undefined){
+                if (userID[0] === undefined) {
                     sqlFunctions.saveNewUser(knex, {
                         htmlFunctions: index,
                         userDetails: data,
                         res: res
                     })
+                } else {
+                    index.thisCombinationAlreadyExist(res);
                 }
             })
-        
+
     } else {
-        index.passNotCorrect(res, {
-            message: "Passwords don't match",
-            data
-        })
+        index.passNotCorrect(res, data)
     }
 });
 
